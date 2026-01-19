acbba26fbb6cf3db6ba1e748c53c2d00d47d66f4
Lucas D. <lucas.desouza.p@gmail.com>
2026-01-19 11:20:32 +0100

    add colors pannels to global.css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const DM_sans = DM_sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -18,9 +14,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${DM_sans.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
 
 
 export default function Home() {
-  
+
   return (
 
-<div>
-    <p>toto</p>
-</div>
-  )}
+    <div>
+      <p>toto</p>
+    </div>
+  )
+}
